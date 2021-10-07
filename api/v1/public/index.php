6396338c7c0084b3b47a4f66a9bc6ae22256b8b8
R4JxF60 <rajithakumaraprog@gmail.com>
2021-10-07 11:11:17 +0530

    refactor the routes

--- api/v1/public/index.php
+++ api/v1/public/index.php
@@ -11,18 +11,22 @@ Router\Router::get("/", function(){
     echo "<h1>RooT</h1>";
 });
 
+// unsecured routes, these routes don't require auth api
 Router\Router::post("/users/casual/signup", Controllers\CasualController::class."::signUp", false);
-
 Router\Router::post("/users/celebrity/signup", Controllers\CelebrityController::class."::signUp", false);
-
 Router\Router::post("/users/organization/signup", Controllers\OrganizationController::class."::signUp", false);
-
-Router\Router::get("/users", Controllers\UserController::class."::checkUsername");
-
 Router\Router::post("/users/login", Controllers\UserController::class."::login", false);
-Router\Router::post("/users", Controllers\UserController::class."::sayHello");
+
+// partially use auth functionalities
 Router\Router::post("/users/silentAuth", Controllers\AuthController::class."::silentAuthenticate", false);
+
+// secured routes for testing purposes
+Router\Router::get("/users", Controllers\UserController::class."::checkUsername");
+Router\Router::post("/users", Controllers\UserController::class."::sayHello");
+
 
 Router\Router::listen(function($port){
     echo "<br><h1>Server up and running on port: $port</h1><br>";
 });
+
+
